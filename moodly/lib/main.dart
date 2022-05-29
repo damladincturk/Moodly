5893c8aa920a646adfe3b9fb161685fe7d4206ce
Damla Dinçtürk <damladnt97@gmail.com>
2022-05-30 00:14:53 +0300

    .

--- moodly/lib/main.dart
+++ moodly/lib/main.dart
@@ -1,7 +1,3 @@
-import 'dart:ffi';
-import 'dart:html';
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:moodly/pages/ayarlar.dart';
